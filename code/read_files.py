d9dc99c5ba5e5630d5a7d8cad782907570099c05
rovenna2021 <ytc17@sfu.ca>
2022-01-23 15:54:48 -0800

    Initial version - Static Page for SRKW call catalogue browsing
    Static page – items will be rendered using backend python scripts in the future (including the paging part)
    1. Main Page
    - searching and filtering (without paging)
    - populate call catalogue items with extracted data
    a. inline audio player
    b. click to open pop up for details
    c. audio player in pop up windows
    2. Backend python program changes to extract call item elements

--- code/read_files.py
+++ code/read_files.py
@@ -11,12 +11,25 @@ try:
     script_folder = os.path.dirname(__file__)
 except NameError:
     script_folder = "."
-data_folder = f"{script_folder}/../SRKW catalogue - J clan"
+data_folder = f"{script_folder}SRKW catalogue - J clan"
 
 def get_filenames(folder):
+    print(folder)
+    names = []
     for dirpath, dirnames, filenames in os.walk(folder):
+        print(dirpath)
+        print(filenames)
+        for name in filenames:
+            names.append(name)
         break
-    return filenames
+    return names
+
+def make_row_html(r):
+    return (
+f"""<figure>
+    <img src="{r.filename}.jpg"/>
+    Pod: {r.pod}
+</figure>""")
 
 def read_data_folder(data_folder):
     filenames = get_filenames(data_folder)
@@ -26,17 +39,19 @@ def read_data_folder(data_folder):
     assert all(df.groupby('filename').size() == 2), "There are unmatched .jpg/.wav file pairs"
     # drop jpg/wav filetype, so each call appears only once in df
     df = df[['filename']].drop_duplicates()
+    
+    df['thumb'] = df['filename'] + '.jpg'
+    df['clan'] = 'J'
 
-    df[['callname','pods','matrilines']] = df['filename'].str.split("-", n=2, expand=True)
+    df[['cn','pod','mar']] = df['filename'].str.split("-", n=2, expand=True)
+    df['pod_cat'] =  df['pod'].str.findall(r'[J|K|L]')
+    #df['html'] = df.apply(make_row_html, axis=1)
     return df
 
 df = read_data_folder(data_folder)
+df.to_csv('srkw_call_data.csv')
+with open('srkw_call_data.json', 'w') as f:
+    f.write(df.to_json(orient='records'))
 
-def make_row_html(r):
-    return (
-f"""<figure>
-    <img src="{r.filename}.jpg"/>
-    Pod: {r.pods}
-</figure>""")
 
 print("\n".join(df.apply(make_row_html, axis=1)))
