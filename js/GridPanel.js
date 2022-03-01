fe2004a5c9a3da956cd9b32b8edf973fe2ca76ee
rovenna2021 <ytc17@sfu.ca>
2022-03-01 14:37:12 +0800

    Catering 12:7 browser window size on individual detail page

--- js/GridPanel.js
+++ js/GridPanel.js
@@ -447,7 +447,7 @@ var GridPanel = undefined;
                 //something went wrong
                 document.location.href = page_link;
             }
-            $('.lity-container').append('<div class="container"><div class="row"><button id="play" class="col btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">\
+            $('.lity-container').append('<div class="container litybottom"><div class="row"><button id="play" class="col btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">\
             <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/>\
           </svg>Play (Call Name: '+lity_data.cn+') </button></div></div>');
             $("#play").off('click').on('click', function(){
