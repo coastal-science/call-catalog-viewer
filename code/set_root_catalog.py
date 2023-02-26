8d4d7bc900b4266ee34be34a21d3ccbfdf96ba1a
Evan <74132466+EvanDyce@users.noreply.github.com>
2023-02-25 22:48:17 -0800

    Adding set_root_catalog functionality to catalog

---
+++ code/set_root_catalog.py
@@ -0,0 +1,132 @@
+# check if the file is already the root catalog, if it is, then leave it
+# if it isn't get user confirmation that this will reset everything and they will have to readd all of them
+    # This means 
+
+
+'''
+- Check if the repo is already the root catalog
+    - If it is, then we can print and exit
+    
+- If it is not the root catalog, ask for user confirmation that they will have to readd all of the old catalogs
+    - Can add functionality to add_remote_catalog to check if repo is already there, and then just add it to library and index??
+    
+    - If user confirms, we need to remove the symlink and the file from the current root_catalog, and then create a new one
+        - New we can borrow functions from add_remote_catalog and create it with itself
+        - Will also have to clear out index.yaml and just have itself. Can steal this too?
+    - If user does not confirm, then we need to just exit
+'''
+
+from os.path import exists, dirname, realpath
+from os import remove, symlink
+from pathlib import Path
+import yaml
+import argparse
+
+REPO_NAME = ''
+CATALOG_PATH = ''
+REPO_ROOT_PATH = ''
+def is_root_catalog(repo_path):
+    return exists(f'{repo_path}/library.yaml')
+
+def retrieve_old_root_data():
+    print(f'Retrieving data from old root catalog...')
+    
+    root_path = CATALOG_PATH + '/library.yaml'
+    with open(root_path, 'r') as f:
+        old_data = yaml.safe_load(f)
+    
+    print('Succesfully retrieved old root catalog data', end='\n\n')
+    return old_data    
+
+def remove_old_library():
+    print('Removing symlink and old library.yaml from root catalog')
+    link_path = Path(CATALOG_PATH + '/library.yaml')
+    real_path = realpath(link_path)
+    
+    # remove symlink
+    link_path.unlink(missing_ok=True)
+    
+    # delete old file
+    try:
+        remove(real_path)
+    except:
+        print('There was a problem removing the old library.yaml file. Exiting program')
+        exit(-1)
+    
+    print('Successfully remove symlink and old library file', end='\n\n')
+    
+def create_new_files(old_library_data):
+    print(f'Creating library.yaml in {REPO_NAME}...')
+    with open(REPO_ROOT_PATH + '/library.yaml', 'w') as f:
+        yaml.dump(old_library_data, f)
+    print(f'Succesfully created library.yaml in {REPO_NAME}')
+    
+    print(f'Creating symlink to catalogs/library.yaml from {REPO_NAME}/library.yaml...')
+    symlink(REPO_ROOT_PATH + '/library.yaml', CATALOG_PATH + '/library.yaml')
+    print(f'Succesfully created symlink to catalogs/library.yaml', end='\n\n')
+    # with open(repo_root_path + '/library.yaml', 'w') as f:
+    #     catalogs = {'catalogs': [url]}
+    #     yaml.dump(catalogs, f)
+    # print(f'Succesfully created library.yaml in {repo_name}')
+    
+    # print(f'Creating symlink from {repo_name}/library.yaml to catalogs/library.yaml...')
+    # symlink(repo_root_path + '/library.yaml', catalogs_path + '/library.yaml')
+    # print(f'Succesfully created symlink to {repo_name}/library.yaml', end='\n\n')
+    # pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Set a new root catalog for viewer',
+        allow_abbrev=True
+    )
+    
+    parser.add_argument(
+        'repo_name',
+        help='Repo name to set as the root catalog'
+    )
+    
+    args = parser.parse_args()
+    
+    REPO_NAME = args.repo_name
+    
+    # create variables
+    CATALOG_PATH = dirname(dirname(__file__)) + '/catalogs'
+    REPO_ROOT_PATH = CATALOG_PATH + '/' + REPO_NAME
+    
+    # check if the repo actually exists
+    if not exists(REPO_ROOT_PATH):
+        print(f'The repo {REPO_NAME} does not exist, cannot set it as root catalog.')
+        exit(-1)
+        
+    # check if the repo is already the root catalog
+    if is_root_catalog(REPO_ROOT_PATH):
+        print(f'The repo {REPO_NAME} is already the root catalog. Doing nothing')
+        exit()
+        
+    # open the old root catalog and store the old data
+    old_catalog = retrieve_old_root_data()
+
+    # removing the symlink and deleting the old file
+    remove_old_library()
+    
+    # create new files and symlnks
+    create_new_files(old_catalog)
+    
+    print(f'Succesfully set {REPO_NAME} as new root catalog')
+    # get user confirmation about what it will do
+    # Delete the old library.yaml, rmeove all old things, and have to readd them
+    # user_response = input(
+    #     f'Setting a new catalog will remove the '
+    # )
+    # print('Please respond with \'y\' if you understand the following consequences')
+    # user_response = input("1. Setting a new root catalog will remove the old root_catatalog/library.yaml")
+    # if user_response not in ('y', 'Y'):
+    #     print(f'User understanding not received. Exiting program')
+    #     exit(-1)
+    
+    # user_response = input("2. Setting a new root catalog will remove all previously added ")
+    # user_response = input(
+    #     f'''Please press 'y' if you understand the following.\n
+    #         1. Setting a new root catalog will remove the old root_catalog/library.yaml and create new symlink.
+    #     '''
+    # )
