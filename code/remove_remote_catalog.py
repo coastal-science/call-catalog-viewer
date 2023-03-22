dc238023e02430fad07add9763f139110e69b940
Evan <74132466+EvanDyce@users.noreply.github.com>
2023-03-21 19:14:50 -0700

    Option to force remove root catalog if it is the last one remaining

--- code/remove_remote_catalog.py
+++ code/remove_remote_catalog.py
@@ -11,6 +11,8 @@ from os import remove
 from shutil import rmtree
 import yaml
 import RemoteUtils
+from pathlib import Path
+
 
 def remove_from_index_yaml(repo_name):
     '''
@@ -75,7 +77,7 @@ def remove_from_library_yaml(repo_name):
 
     print(f'Successfully removed {repo_name} from library.yaml', end='\n\n')
 
-def remove_files(repo_name):
+def remove_catalog_files(repo_name):
     '''
     Remove all of the old files from the catalog that was added
     '''
@@ -90,12 +92,38 @@ def remove_files(repo_name):
     try:
         rmtree(f'{CATALOG_PATH}/{repo_name}')
     except:
-        print(f'Unable to remove direcotyr {CATALOG_PATH}/{repo_name}. Exiting')
+        print(f'Unable to remove directory {CATALOG_PATH}/{repo_name}. Exiting')
         exit(-1)
     
     print(f'Successfully removed all files for repo {repo_name}', end='\n')    
 
-
+def is_last_catalog():
+    path = f'{CATALOG_PATH}/library.yaml'
+    
+    with open(path, 'r+') as f:
+        catalogs = yaml.safe_load(f)
+        
+        if not catalogs:
+            print(f'Nothing to remove in {CATALOG_PATH}/library.yaml. Exiting')
+            exit(-1)
+        
+        return len(catalogs['catalogs']) == 1
+    
+def remove_root_catalog(repo_name):
+    '''
+    Get rid of all catalog files as well as index.yaml and library.yaml
+    '''
+    # removing the catalog specific files and add a newline
+    remove_catalog_files(repo_name)
+    print()
+    
+    print('Removing index.yaml and library.yaml from catalogs directory...')
+    p = Path(CATALOG_PATH, 'library.yaml')
+    p.unlink(missing_ok=True)
+    
+    remove(CATALOG_PATH + '/index.yaml')
+    print('Successfully removed index.yaml and library.yaml')
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -108,9 +136,19 @@ if __name__ == '__main__':
         help='Repo name to be removed'
     )
     
+    parser.add_argument(
+        '--force',
+        help='Force remove the root catalog. Must be the only catalog in the viewer',
+        dest='force',
+        default=False,
+        required=False,
+        action=argparse.BooleanOptionalAction
+    )
+    
     args = parser.parse_args()
     
     repo_name = args.repo_name
+    force_remove = args.force
     
     # extract information
     CATALOG_PATH = dirname(dirname(__file__)) + '/catalogs'
@@ -118,10 +156,19 @@ if __name__ == '__main__':
     
     is_root = RemoteUtils.is_root_catalog(CATALOG_PATH + '/library.yaml', REPO_PATH)
     
-    if is_root:
-        print('Attempting to remove root catalog. Please set a new root catalog before removing this one.')
-        exit(-1)
+    if is_root and force_remove:
+        if not is_last_catalog():
+            print(f'Cannot remove the root catalog {repo_name} while other catalogs are in the viewer. Please remove all other catalogs first.')
+            exit(1)
         
-    remove_from_index_yaml(repo_name)
-    remove_from_library_yaml(repo_name)
-    remove_files(repo_name)
+        remove_root_catalog(repo_name)
+        
+    elif is_root and not force_remove:
+        print('Attempting to remove root catalog. Please set a new root catalog or specify --force.')
+        exit(-1)
+    else:
+        remove_from_index_yaml(repo_name)
+        remove_from_library_yaml(repo_name)
+        remove_catalog_files(repo_name)
+        
+        
