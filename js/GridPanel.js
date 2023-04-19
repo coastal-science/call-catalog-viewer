28a864ba1932dc7361e983e5204fbd72d718f367
VWJF <3881641+VWJF@users.noreply.github.com>
2023-04-19 12:44:03 -0700

    Removed "Unknown" catalog values are not specified

--- js/GridPanel.js
+++ js/GridPanel.js
@@ -177,7 +177,7 @@ var GridPanel = undefined;
         var keys = Object.keys(data);
         keys.forEach(p => {
             if (data[p] == undefined || data[p] == null) {
-                data[p] = "Unknown";
+                data[p] = ""; //"Unknown"
             }
         })
         return data;
@@ -381,13 +381,13 @@ var GridPanel = undefined;
                 searching_para[filterable] = element.slice(1); // add the filterable param to the searching_params
             } else { // filterable is already in the parameters. Add all the elements that are not already in it
                 element.slice(1).forEach(val => {
-                    if (!searching_para[filterable].includes(val)) {
+                    if (!val && !searching_para[filterable].includes(val)) { // not an empty string and doesn't already exist.
                         searching_para[filterable].push(val);
                     }
                 });
             }
             // ensure that the 'Unknown' field is at the bottom of the dropdown
-            var index = searching_para[filterable].indexOf("Unknown");
+            var index = searching_para[filterable].indexOf("Unknown"); //Unknown deprecated with empty string.
             if (index != -1) {
                 searching_para[filterable].splice(index, 1);
                 searching_para[filterable].push('Unknown');
