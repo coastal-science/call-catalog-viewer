fca3a30a1e399de6295cb3cd830c86f2c83075e9
VWJF <3881641+VWJF@users.noreply.github.com>
2023-06-05 04:53:02 -0700

    Add some corner cases for testing

--- tests/TestCornerCases.py
+++ tests/TestCornerCases.py
@@ -36,7 +36,7 @@ desc = "Create a file with 1 line of content space removed"
 test_case = f"""
 corner/{case_id}-{desc}/file.txt:Line 1
 """
-expected = f"corner/{case_id}-{desc}"
+expected = 'Error'
 
 all_tests.append(TestCase(case_id, test_case, expected, desc))
 case_id += 1
@@ -49,9 +49,40 @@ desc = "Create a file with 1 line of content colon removed"
 test_case = f"""
 corner/{case_id}-{desc}/file.txt Line 1
 """
-expected = False
+expected = 'Error'
 
 all_tests.append(TestCase(case_id, test_case, expected, desc))
 case_id += 1
+
+###############
+# TEST CASE 0
+###############
+desc = "Folder name ends in period"
+test_case = f"""
+corner/{case_id}-{desc}/file.:
+"""
+expected = f"corner/{case_id}-{desc}"
+
+all_tests.append(TestCase(case_id, test_case, expected, desc))
+case_id += 1
+
+###############
+# TEST CASE 0
+###############
+desc = "File name ends in period"
+test_case = f"""
+corner/{case_id}-{desc}/file.: |
+                            Level 1:
+                                Level 2:
+                                    - item 1
+                                    - item 2
+                                condition: True
+                                number: 5
+"""
+expected = f"corner/{case_id}-{desc}"
+
+all_tests.append(TestCase(case_id, test_case, expected, desc))
+case_id += 1
+
 
 print(*all_tests, sep='\n', end='\n\n')
