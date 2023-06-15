dc4095c3ecfbc78b7de1dfa2649393726d12ee83
VWJF <3881641+VWJF@users.noreply.github.com>
2023-06-15 00:35:18 -0700

    Change pytest working directory to `tmp_path` and type annotations
    https://stackoverflow.com/a/62055409

--- tests/test_remove_catalog.py
+++ tests/test_remove_catalog.py
@@ -4,8 +4,12 @@ from src.remove_catalog import cli as remove_catalog
 from src.utils import yaml
 from tests.test_add_catalog import make_library, make_index, make_existing
 
-def test_remove_cli_empty():
+
+def test_remove_cli_empty(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
     """Calling cli without arguments should exit and want about missing arguments"""
+    
+    # arrange
+    monkeypatch.chdir(tmp_path)
 
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         exit_code = remove_catalog()
@@ -13,10 +17,12 @@ def test_remove_cli_empty():
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code != 0
 
-def test_remove_from_uninitialized_library(tmp_path: Path, capsys, caplog):
+
+def test_remove_from_uninitialized_library(tmp_path: Path, capsys: pytest.CaptureFixture[str], caplog: pytest.LogCaptureFixture, monkeypatch: pytest.MonkeyPatch):
     """Attempt to remove a catalog from uninitialized index.yaml"""
     
     # arrange
+    monkeypatch.chdir(tmp_path)
 
     # act
     print(f"{tmp_path=}")
@@ -27,6 +33,17 @@ def test_remove_from_uninitialized_library(tmp_path: Path, capsys, caplog):
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code != 0
     
+    # cleanup
+    # operations occur in the `tmp_path` folder,
+    # which is unique to each invocation of the test function handled by `monkeypatch`
+
+
+def test_remove_from_empty_library(tmp_path: Path, capsys: pytest.CaptureFixture[str], caplog: pytest.LogCaptureFixture, monkeypatch: pytest.MonkeyPatch):
+    """Attempt to remove a catalog from empty index.yaml"""
+    
+    # arrange
+    print(f"{tmp_path=}")
+    monkeypatch.chdir(tmp_path)
     # cleanup
     # operations occur in the `tmp_path` folder,
     # which is unique to each invocation of the test function.
