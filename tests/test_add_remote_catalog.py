e61ee6be07dd789e805e81dc919b752343e8de16
Evan <74132466+EvanDyce@users.noreply.github.com>
2023-07-12 23:53:12 -0700

    Removing duplicate test and updating test cases

--- tests/test_add_remote_catalog.py
+++ tests/test_add_remote_catalog.py
@@ -56,7 +56,7 @@ def test_add_cli_empty(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code != 0
     
-def test_invalid_git_url(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+def test_invalid_git_url_format(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
     """Calling cli with an invalid git url"""
     
     # arrange
@@ -66,14 +66,14 @@ def test_invalid_git_url(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         add_remote_catalog([
             "fake-git-url",
-            "call-catalog.yaml"
+            VALID_YAML_FILE
         ])
         
     # assert
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code != 0
     
-def test_invalid_git_url(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
+def test_invalid_yaml_file_format(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
     """Calling cli with an invalid yaml file. Will fail with argparser validation"""
     
     # arrange
@@ -82,32 +82,15 @@ def test_invalid_git_url(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
     # act
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         add_remote_catalog([
-            "real-git-url",
-            "call-catalog.yaml"
-        ])
-        
-    # assert
-    assert pytest_wrapped_e.type == SystemExit
-    assert pytest_wrapped_e.value.code != 0
-    
-def test_invalid_yaml_file(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
-    """Calling cli with an invalid yaml file. Will fail with argparser validation"""
-    
-    # arrange
-    monkeypatch.chdir(tmp_path)
-    
-    # act
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        add_remote_catalog([
-            "real-git-url.git",
-            "call-catalog.fake"
+            VALID_GIT_SSH,
+            "call-data.fake"
         ])
         
     # assert
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code != 0
 
-def test_ssh_yaml_not_exist(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+def test_yaml_not_exist(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
     """Calling cli with a valid ssh git url and a valid yaml filename that is not in repo"""
     # arrange
     monkeypatch.chdir(tmp_path)
@@ -124,6 +107,7 @@ def test_ssh_yaml_not_exist(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, cap
     assert exit_code != 0
     assert f'The yaml file {fake_yaml} does not exist in {VALID_REPO_NAME}' in caplog.text
     
+@pytest.mark.skip
 def test_https_yaml_not_exist(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
     """Calling cli with a valid https git url and a valid yaml filename that is not in repo"""
     # arrange
@@ -141,7 +125,7 @@ def test_https_yaml_not_exist(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, c
     assert exit_code != 0
     assert f'The yaml file {fake_yaml} does not exist in {VALID_REPO_NAME}' in caplog.text
     
-def test_ssh_yaml_exists(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
+def test_yaml_exists(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
     """Adding first catalog with valid arguments. Validate correct file creation"""
     # arrange
     monkeypatch.chdir(tmp_path)
