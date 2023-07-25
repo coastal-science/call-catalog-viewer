8add113da066e6a4ada50272d4a7b7d109d793a3
Evan <74132466+EvanDyce@users.noreply.github.com>
2023-07-24 23:28:19 -0700

    Refactoring catalog_versions with cli and logger

--- src/catalog_versions.py
+++ src/catalog_versions.py
@@ -15,25 +15,30 @@ from os.path import dirname, exists
 from git import Repo
 import utils
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+CATALOG_VERSIONS_ERROR = -1
 
 def list_versions(repo):
     '''
     List all of the versions available for the repo. Lists all tags that have been pulled to local
     '''   
     if len(repo.tags) == 0:
-        print('Not versions available for the catalog. View documentation for instructions to create')
-        exit(-1)
+        logger.error('Not versions available for the catalog. View documentation for instructions to create')
+        return CATALOG_VERSIONS_ERROR
         
     for v in repo.tags:
-        print(v)
+        logger.info(v)
 
 def tag_exists(repo, tag):
     '''
     Check whether a tag exists for a specific repository
     '''   
     if len(repo.tags) == 0:
-        print('Not versions available for the catalog. View documentation for instructions to create')
-        exit(-1)
+        logger.error('Not versions available for the catalog. View documentation for instructions to create')
+        return CATALOG_VERSIONS_ERROR
     
     found = False
     for t in repo.tags:
@@ -43,20 +48,20 @@ def tag_exists(repo, tag):
     
     return found
 
-def rebuild_files():
+def rebuild_files(path_to_catalogs_dir, catalog_name):
     '''
     Update the files after a different tag has been checked out
     '''
     # fetch the yaml file name from the json since we know json, but not yaml all the time. Stops from having to specify
-    with open(CATALOGS_PATH + '/' + REPO_NAME + '.json') as f:
+    with open(path_to_catalogs_dir + '/' + catalog_name + '.json') as f:
         data = json.load(f)
         yaml_file = data['yaml-file']
     
     # pass the yaml file to create the new json
-    df, population, filter, sortables, display, site_details = utils.parse_yaml_to_json(CATALOGS_PATH, CATALOGS_PATH + '/' + REPO_NAME + '/' + yaml_file)
-    utils.export_to_json(CATALOGS_PATH, df, population, filter, sortables, display, site_details, REPO_NAME, yaml_file)
+    df, population, filter, sortables, display, site_details = utils.parse_yaml_to_json(path_to_catalogs_dir, path_to_catalogs_dir + '/' + catalog_name + '/' + yaml_file)
+    utils.export_to_json(path_to_catalogs_dir, df, population, filter, sortables, display, site_details, catalog_name, yaml_file)
     
-if __name__ == '__main__':
+def cli(args=None):
     parser = argparse.ArgumentParser(
         description='Change the versions of a remote catalogs', 
         allow_abbrev=True
@@ -94,39 +99,45 @@ if __name__ == '__main__':
         action=argparse.BooleanOptionalAction
     )
     
-    args = parser.parse_args()
+    parser.add_argument(
+        '--path',
+        default="default",
+        required=False,
+        help='Optional paramater to override location of catalogs directory. Default will be ../../catalogs/'
+    )
+    
+    args = parser.parse_args(args)
     
     catalog_name = args.catalog_name
     version = args.version
     list_all = args.list_versions
     latest = args.latest_version
     
-    CATALOGS_PATH = dirname(dirname(__file__)) + '/catalogs'
-    REPO_NAME = catalog_name
+    path_to_catalogs_dir = args.path if args.path != 'default' else dirname(dirname(__file__)) + '/catalogs'
     
     # catalog name was not specified
     if catalog_name == 'no_repo':
-        print('The name of the catalog must be specified')
-        exit(-1)
+        logger.error('The name of the catalog must be specified')
+        return CATALOG_VERSIONS_ERROR
         
     # specified catalog does not exist
-    if not exists(CATALOGS_PATH + '/' + catalog_name):
-        print(f'The catalog {catalog_name} does not exist. Please enter a valid catalog name')
-        exit(-1)
+    if not exists(path_to_catalogs_dir + '/' + catalog_name):
+        logger.error(f'The catalog {catalog_name} does not exist. Please enter a valid catalog name')
+        return CATALOG_VERSIONS_ERROR
     
     # we have a valid catalog, so we can set the path and create the repo reference
-    ROOT_REPO_PATH = CATALOGS_PATH + '/' + catalog_name
+    ROOT_REPO_PATH = path_to_catalogs_dir + '/' + catalog_name
     catalog_repo = Repo(ROOT_REPO_PATH)
     
     # didn't specify what action to do
     if version == 'no_version' and not list_all and not latest:
-        print('Please specify the version or use --list or --latest')
-        exit(-1)
+        logger.error('Please specify the version or use --list or --latest')
+        return CATALOG_VERSIONS_ERROR
         
     # specified more than one action
     if (version != 'no_version' and list_all) or (version != 'no_version' and latest) or (latest and list_all):
-        print('Please specify only one of catalog version, --list, or --latest')
-        exit(-1)
+        logger.error('Please specify only one of catalog version, --list, or --latest')
+        return CATALOG_VERSIONS_ERROR
         
     # we are down to have only one valid action and can handle all of them
     if list_all:
@@ -134,11 +145,16 @@ if __name__ == '__main__':
     elif latest:
         # checkout the main to avoid weird git states and conflicts
         catalog_repo.git.checkout('main')
-        rebuild_files()
+        rebuild_files(path_to_catalogs_dir, catalog_name)
     else:
         if not tag_exists(catalog_repo, version):
-            print(f'The version {version} does not exist. To view available options use --list')
-            exit(-1)
+            logger.error(f'The version {version} does not exist. To view available options use --list')
+            return CATALOG_VERSIONS_ERROR
             
         catalog_repo.git.checkout(version)
-        rebuild_files()
+        rebuild_files(path_to_catalogs_dir, catalog_name)
+
+    return 0
+
+if __name__ == '__main__':
+    cli()
