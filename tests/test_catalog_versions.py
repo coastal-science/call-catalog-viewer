a47d29d180ade003f0f4ba8b85b6966121285f7e
Evan <74132466+EvanDyce@users.noreply.github.com>
2023-07-27 00:44:11 -0700

    Adding descriptors to all of catalog_versions.py tests

--- tests/test_catalog_versions.py
+++ tests/test_catalog_versions.py
@@ -8,6 +8,7 @@ from pathlib import Path
 REAL_CATALOG_NAME = "catalog-versions-test"
 
 def test_empty_cli(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    '''Calling with no arguments'''
     # arrange
     monkeypatch.chdir(tmp_path)
     
@@ -18,6 +19,7 @@ def test_empty_cli(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pyte
     assert 'The name of the catalog must be specified' in caplog.messages
     
 def test_catalog_does_not_exist(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    '''Calling with a catalog name that is not real'''
     # arrange
     monkeypatch.chdir(tmp_path)
     not_real_catalog = "not-real-catalog"
@@ -31,6 +33,7 @@ def test_catalog_does_not_exist(tmp_path: Path, shared_datadir: Path, monkeypatc
     assert f'The catalog {not_real_catalog} does not exist. Please enter a valid catalog name' in caplog.messages
     
 def test_no_action_specified(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    '''Calling without an action (latest, list, version)'''
     # arrange
     monkeypatch.chdir(tmp_path)
     
@@ -45,6 +48,7 @@ def test_no_action_specified(tmp_path: Path, shared_datadir: Path, monkeypatch: 
     assert 'Please specify the version or use --list or --latest' in caplog.messages
 
 def test_specify_list_and_latest(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    '''Calling specifying both list and latest, should fail'''
     # arrange
     monkeypatch.chdir(tmp_path)
     
@@ -61,6 +65,7 @@ def test_specify_list_and_latest(tmp_path: Path, shared_datadir: Path, monkeypat
     assert 'Please specify only one of catalog version, --list, or --latest' in caplog.messages
 
 def tests_specify_version_and_list(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    '''Calling specifying both a version and list action, should fail'''
     # arrange
     monkeypatch.chdir(tmp_path)
     
@@ -74,9 +79,10 @@ def tests_specify_version_and_list(tmp_path: Path, shared_datadir: Path, monkeyp
     
     # assert 
     assert EXIT_CODE != 0
-    assert 'Please specify only one of catalog version, --list, or --latest'
+    assert 'Please specify only one of catalog version, --list, or --latest' in caplog.messages
 
 def tests_specify_version_and_latest(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    '''Calling specifying both version and latest, should fail'''
     # arrange
     monkeypatch.chdir(tmp_path)
     
@@ -90,11 +96,13 @@ def tests_specify_version_and_latest(tmp_path: Path, shared_datadir: Path, monke
     
     # assert 
     assert EXIT_CODE != 0
-    assert 'Please specify only one of catalog version, --list, or --latest'
+    assert 'Please specify only one of catalog version, --list, or --latest' in caplog.messages
     
 def test_list_tags(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    '''Calling with --list to list all of the tags'''
     # arrange
     monkeypatch.chdir(tmp_path)
+    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/')  # create our copy of the local repository so we don't mess it up for future testing
     caplog.set_level(logging.INFO)
     
     # act
@@ -112,9 +120,10 @@ def test_list_tags(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.Mon
     assert 'v3.0' in caplog.messages
     
 def test_checkout_v1(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch):
+    '''Checking out the v1.0 tag and validating file content'''
     # arrange 
     monkeypatch.chdir(tmp_path)
-    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up with things
+    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up for future testing
     
     
     # act
@@ -131,9 +140,10 @@ def test_checkout_v1(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.M
         assert f.readline() == '1'
         
 def test_checkout_v2(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch):
+    '''Checking out the v2.0 tag and validating file content'''
     # arrange 
     monkeypatch.chdir(tmp_path)
-    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up with things
+    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up for future testing
     
     
     # act
@@ -150,9 +160,10 @@ def test_checkout_v2(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.M
         assert f.readline() == '2'
 
 def test_checkout_v3(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch):
+    '''Checking out the v3.0 tag and validating file content'''
     # arrange 
     monkeypatch.chdir(tmp_path)
-    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up with things
+    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up for future testing
     
     
     # act
@@ -169,9 +180,10 @@ def test_checkout_v3(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.M
         assert f.readline() == '3'
         
 def test_checkout_latest(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch):
+    '''Checking out --latest (v3.0) and verifying file content'''
     # arrange 
     monkeypatch.chdir(tmp_path)
-    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up with things
+    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up for future testing
     
     
     # act
@@ -188,9 +200,10 @@ def test_checkout_latest(tmp_path: Path, shared_datadir: Path, monkeypatch: pyte
         assert f.readline() == '3'
   
 def test_checkout_multiple_versions_in_sequence(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch):
+    '''Checking out v3.0, verifying content, and then repeating for v2.0 with same files'''
     # arrange 
     monkeypatch.chdir(tmp_path)
-    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up with things
+    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up for future testing
     first_version = 'v3.0'
     second_version = 'v2.0'
     
@@ -218,9 +231,10 @@ def test_checkout_multiple_versions_in_sequence(tmp_path: Path, shared_datadir: 
         assert f.readline() == '2'
 
 def test_checkout_version_then_latest(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch):
+    '''Checking out v1.0 and then --latest, verifying file content for both'''
     # arrange 
     monkeypatch.chdir(tmp_path)
-    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up with things
+    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up for future testing
     first_version = 'v1.0'
     
     # act
@@ -247,9 +261,10 @@ def test_checkout_version_then_latest(tmp_path: Path, shared_datadir: Path, monk
         assert f.readline() == '3'
 
 def test_checkout_invalid_tag(tmp_path: Path, shared_datadir: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    '''Attempting to checkout an invalid tag'''
     # arrange 
     monkeypatch.chdir(tmp_path)
-    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up with things
+    copy_tree(str(shared_datadir) + '/catalogs/', str(tmp_path) + '/catalogs/') # create our copy of the local repository so we don't mess it up for future testing
     invalid_version = '5.0'
     
     # act
