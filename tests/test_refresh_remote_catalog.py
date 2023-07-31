01676541a0b8cf525696fa5c61c726a4c8797b70
Evan <74132466+EvanDyce@users.noreply.github.com>
2023-07-30 18:38:35 -0700

    Adding tests to refresh remote catalog

--- tests/test_refresh_remote_catalog.py
+++ tests/test_refresh_remote_catalog.py
@@ -1,9 +1,7 @@
 import pytest
 from pathlib import Path
-from unittest.mock import patch
 from src.refresh_remote_catalog import cli as refresh_remote_catalog
 from testing_utils import dummy_remote_add, dummy_local_add
-
 
 def test_refresh_cli_empty(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
     """Calling cli without arguments should fail and warn about missing arguments"""
@@ -69,3 +67,19 @@ def test_refresh_local_catalog(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, 
     # assert
     assert EXIT_CODE != 0
     assert f'The catalog {local_name} is not a remote catalog' in caplog.messages
+    
+def test_invalid_git_repo(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    # arrange
+    monkeypatch.chdir(tmp_path)
+    remote_url, remote_name = 'fake-repo.git', 'fake-repo'
+    dummy_remote_add(remote_url, remote_name, tmp_path)
+    
+    # act
+    EXIT_CODE = refresh_remote_catalog([
+        remote_name,
+        "--path", str(tmp_path)
+    ])
+    
+    # assert
+    assert EXIT_CODE != 0
+    assert f'There was a problem pulling the changes for repo {remote_name}' in caplog.messages
