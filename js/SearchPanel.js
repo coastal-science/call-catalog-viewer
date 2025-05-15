36b84a134d654c15796d39f33545dd9f78ad672a
VWJF <3881641+VWJF@users.noreply.github.com>
2025-05-14 19:43:40 -0700

    Update SearchPanel.js validate dropdown selection `s1` for population/catalogue before updating the url search params

--- js/SearchPanel.js
+++ js/SearchPanel.js
@@ -328,7 +328,9 @@ var SearchPanel = undefined;
                     }
                     buildPopulationSpecificDropdown(selected_population);
                     updateURL('sel', liveDropdownChoices);
-                    updateURL('catalogue', selected_population)
+                    if (selected_population){
+                        updateURL('catalogue', selected_population)
+                    }
                     GridPanel.get_new(liveDropdownChoices);
                 }
             });
